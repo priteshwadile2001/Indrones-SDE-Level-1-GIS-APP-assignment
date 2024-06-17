f2fa4eda63ec8cf5386b353275e4330429172189
Pritesh Wadile <85450739+priteshwadile2001@users.noreply.github.com>
2024-06-17 11:51:36 +0530

    Update seriliazers.py

--- gis_project/gis_app/seriliazers.py
+++ gis_project/gis_app/seriliazers.py
@@ -8,23 +8,23 @@ class LocationSerializer(serializers.ModelSerializer):
         model = Location
         fields = "__all__"
 
-    # def create(self, validated_data):
-    #     coordinates_data = validated_data.pop('coordinates')
-    #     # Create a Point object from the coordinates
-    #     point = Point(coordinates_data[0], coordinates_data[1])
-    #     validated_data['coordinates'] = point
-    #     print(validated_data,"****************")
-    #     instance = super().create(validated_data)
-    #     return instance
+    def create(self, validated_data):
+        coordinates_data = validated_data.pop('coordinates')
+        # Create a Point object from the coordinates
+        point = Point(coordinates_data[0], coordinates_data[1])
+        validated_data['coordinates'] = point
+        print(validated_data,"****************")
+        instance = super().create(validated_data)
+        return instance
 
 class BoundarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Boundary
         fields = '__all__'
 
-    # def create(self, validated_data):
-    #     area_str = validated_data.pop('area')
-    #     # Assuming the area is provided as a WKT string
-    #     area = Polygon.from_ewkt(area_str)
-    #     validated_data['area'] = area
-    #     return super().create(validated_data)
+    def create(self, validated_data):
+        area_str = validated_data.pop('area')
+        # Assuming the area is provided as a WKT string
+        area = Polygon.from_ewkt(area_str)
+        validated_data['area'] = area
+        return super().create(validated_data)
