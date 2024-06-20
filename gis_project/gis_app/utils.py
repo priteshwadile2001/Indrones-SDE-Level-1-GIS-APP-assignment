2e68c6650147ca6036ddcb0ca82a66be17e4cb35
Pritesh Wadile <85450739+priteshwadile2001@users.noreply.github.com>
2024-06-20 13:22:02 +0530

    exception handler

--- gis_project/gis_app/utils.py
+++ gis_project/gis_app/utils.py
@@ -1,9 +1,24 @@
+from rest_framework.views import exception_handler  # Importing exception_handler from Django REST Framework views
+from rest_framework.exceptions import AuthenticationFailed  # Importing AuthenticationFailed exception from DRF
+from rest_framework.response import Response  # Importing Response class from DRF
 import csv  # Importing CSV module for CSV file operations
 from io import TextIOWrapper  # Importing TextIOWrapper from io module for file decoding
 from django.contrib.gis.geos import Point  # Importing Point from Django GIS
 from .models import Location  # Importing Location model from current directory's models module
 
 # Custom exception handler for handling specific exceptions
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)  # Default exception handler from DRF
+    
+    if isinstance(exc, AuthenticationFailed) and 'Session expired' in str(exc):
+        # Handling AuthenticationFailed exception with specific message
+        response_data = {
+            'error': 'Session expired',
+            'message': 'Your session has expired. Please refresh the page to continue.'
+        }
+        return Response(response_data, status=401)  # Returning customized response with status code 401
+    
+    return response  # Returning default response if no custom handling is required
 
 # Function to process uploaded CSV file
 def process_csv_file(file):
@@ -51,3 +66,6 @@ def process_csv_file(file):
     
     except Exception as e:
         raise e  # Re-raising any exceptions encountered during processing
+
+
+
