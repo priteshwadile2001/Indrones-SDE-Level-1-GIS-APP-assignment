8b4f9e3ce1d099dfb0d3110d85441224c05d3420
Pritesh Wadile <85450739+priteshwadile2001@users.noreply.github.com>
2024-06-23 20:15:06 +0530

    Update urls.py

--- gis_project/gis_app/urls.py
+++ gis_project/gis_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include  # Importing path and include functions from Django's URL library
 from rest_framework.routers import DefaultRouter  # Importing DefaultRouter from Django REST Framework
 from gis_app.views import (  # Importing views from gis_app.views module
-    LocationViewSet, BoundaryViewSet, BoundaryDetailAPIView,LocationDetailAPIView,
+    LocationViewSet, BoundaryViewSet, 
     calculate_distance,CheckBoundaryView,
 )
 from . import views  # Importing views from the current directory (assuming views.py is in the same directory)
@@ -13,15 +13,11 @@ router.register(r'boundaries', BoundaryViewSet)  # Registering BoundaryViewSet w
 
 urlpatterns = [
     path('', include(router.urls)),  # Including router URLs (automatically includes URLs for 'locations' and 'boundaries')
-    path('locations/<int:pk>/', LocationDetailAPIView.as_view(), name='location-detail'),
-    path('boundaries/', BoundaryDetailAPIView.as_view(), name='boundary-detail'),  # URL pattern for BoundaryDetailAPIView
     path('check-boundary/', CheckBoundaryView.as_view(), name='check-boundary'),
     path('map/', views.map_view, name='map-view'),  # URL pattern for map view
     path('calculate_distance/', calculate_distance, name='calculate_distance'),  # URL pattern for calculate_distance API endpoint
     path('auth/', include('rest_framework.urls')),  # URL patterns for authentication views provided by Django REST Framework
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  # URL pattern for login view with custom template
     path('accounts/profile/', views.profile_view, name='profile'),  # URL pattern for profile view
-   
-   
    
 ]
